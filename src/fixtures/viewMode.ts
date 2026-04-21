353bef62eeb1e5c360bab34dc2b281ae300f3b49
kavos113 <kohei.hadano@gmail.com>
2026-04-21 18:51:43 +0900

    remove old files

--- src/fixtures/viewMode.ts
+++
@@ -1,1 +0,0 @@
-export type ViewMode = 'editor' | 'split' | 'preview';
--- src/renderer/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import { createPinia } from 'pinia';
-import './assets/main.css';
-
-const pinia = createPinia();
-
-createApp(App).use(pinia).mount('#app');
